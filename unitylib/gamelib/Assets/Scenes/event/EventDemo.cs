5c9e70b8e9e007c86c7ed8700a2ce85af730259d
FlyTouch <75076304+FlyTouch@users.noreply.github.com>
2020-11-27 15:57:22 +0800

    增加事件机制 EventManager

---
+++ unitylib/gamelib/Assets/Scenes/event/EventDemo.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventDemo : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        EventManager.Instance.AddEvent("hd", delegate (object obj) {
+
+            Debug.Log(obj);
+        });
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            EventManager.Instance.TriggerEvent("hd","我很好的");
+        }
+        
+    }
+}
---
+++ unitylib/gamelib/Assets/script/lib/manager/util/Singleton.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public sealed  class Singleton  
+{
+    private static Singleton _instance = null;
+    private static readonly object SynObject = new object();
+
+    Singleton()
+    {
+    }
+
+    /// <summary>
+    /// Gets the instance.
+    /// </summary>
+    public static Singleton Instance
+    {
+        get
+        {
+            // Syn operation.
+            lock (SynObject)
+            {
+                return _instance ?? (_instance = new Singleton());
+            }
+        }
+    }
+}
